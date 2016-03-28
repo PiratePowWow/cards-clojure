01c38422e460f7b675d0135580a316a8110d71cd
PiratePowWow <Thi3v3ry>
2016-03-28 17:21:18 -0400

    9-1 Complete

--- src/cards_clojure/core.clj
+++ src/cards_clojure/core.clj
@@ -10,7 +10,7 @@
     (for [suit suits
           rank ranks]
       {:suit suit
-       :rank (get rank-names rank rank)})))
+       :rank rank})))
 
 (defn create-hands [deck]
   (set
@@ -23,8 +23,35 @@
 (defn flush? [hand]
   (= 1 (count (set (map :suit hand)))))
 
+(defn two-pair? [hand]
+  (let [eachRank (frequencies (map :rank hand))
+        twoPair? (= '(2 2) (vals eachRank))]
+   twoPair?))
+
+(defn three-of-a-kind? [hand]
+  (let [eachRank (frequencies (map :rank hand))
+        three? (contains? (set (vals eachRank)) 3)]
+    three?))
+
+(defn straight? [hand]
+  (let [eachRank (vec (sort (set (map :rank hand))))]
+    (cond
+      (= 4 (count eachRank))
+      (cond
+        (and (not= (get eachRank 0) 1)
+          (= 3 (- (get eachRank 3) (get eachRank 0))))
+        true
+        (and (= 1 (get eachRank 0))
+          (= 3 (- (get eachRank 3) (get eachRank 0))))
+        true
+        (and (= 1 (get eachRank 0))
+          (= 37 (apply + eachRank)))
+        true
+        :else false))))
+        
 (defn -main []
-  (let [deck (create-deck)
-        hands (create-hands deck)
-        flush-hands (filter flush? hands)]
-    (count flush-hands)))
+  (time
+    (let [deck (create-deck)
+          hands (create-hands deck)
+          flush-hands (filter straight? hands)]
+      (count flush-hands))))
